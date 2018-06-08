a6364c8198188d184a43c7dea457faf21900103e
Lorena <lorena.giddens@outlook.com>
2018-06-08 16:18:02 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -13,8 +13,12 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
+
 def remove_strawberry(contacts)
+  # 1: iterate over the first level
   contacts.each do |person, contact_details_hash|
+    # 2: iterate over the second level
+    contact_details_hash.each do |attribute, data|
     binding.pry
+  end
 end
